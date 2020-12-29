c85e86fc136dded3f2e61acdae2e8d0feb9d2b0c
Rubèn <numbworks@gmail.com>
2020-12-29 20:39:59 +0100

    LabeledTextJsonDeserializer, I*: removed Outcome dependency.

---
+++ src/NW.NGrams/LabeledText/ILabeledTextJsonDeserializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace NW.NGrams
+{
+    public interface ILabeledTextJsonDeserializer
+    {
+
+        /// <summary>
+        /// It expects a JSON string that looks like: { "LabeledTextId": 1, "Label": "sv", "Text": "Vår kund erbjuder..." }.
+        /// </summary>
+        List<LabeledTextJson> Do(string json);
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 29.12.2020
+
+*/
