61f905198b7452f037bb5b452aef69903a095e14
Rubèn <numbworks@gmail.com>
2020-12-30 01:40:26 +0100

    LabeledTextSimilarityIndex, LabeledTextSimilarityAverage: removed hungarian notation, removed setters.

---
+++ src/NW.NGrams/LabeledText/LabeledTextSimilarityAverage.cs
@@ -0,0 +1,44 @@
+﻿namespace NW.NGrams
+{
+    public class LabeledTextSimilarityAverage : ILabeledTextSimilarityValue
+    {
+
+        // Fields
+        // Properties
+        public string Label { get; }
+        public double Average { get; }
+
+        // Constructors
+        public LabeledTextSimilarityAverage(string label, double average)
+        {
+
+            Label = label;
+            Average = average;
+
+        }
+
+        // Methods
+        public string ToHeader()
+        {
+
+            /* Label   Average */
+            return string.Format("{0}\t{1}", nameof(Label), nameof(Average));
+
+        }
+        public override string ToString()
+        {
+
+            /* 1   sv   */
+            return string.Format("{0}\t{1}",Label, Average.ToString());
+
+        }
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 29.12.2020
+
+*/
---
+++ src/NW.NGrams/LabeledText/LabeledTextSimilarityIndex.cs
@@ -0,0 +1,46 @@
+﻿namespace NW.NGrams
+{
+    public class LabeledTextSimilarityIndex : ILabeledTextSimilarityValue
+    {
+
+        // Fields
+        // Properties
+        public ulong LabeledTextId { get; }
+        public string Label { get; }
+        public double SimilarityIndex { get; }
+
+        // Constructors
+        public LabeledTextSimilarityIndex(ulong id, string label, double similarityIndex)
+        {
+
+            LabeledTextId = id;
+            Label = label;
+            SimilarityIndex = similarityIndex; 
+
+        }
+
+        // Methods
+        public string ToHeader()
+        {
+
+            /* LabeledTextId   Label   SimilarityIndex */
+            return string.Format("{0}\t{1}\t{2}", nameof(LabeledTextId), nameof(Label), nameof(SimilarityIndex));
+
+        }
+        public override string ToString()
+        {
+
+            /* 1   sv   0.89 */
+            return string.Format("{0}\t{1}\t{2}", LabeledTextId.ToString(), Label, SimilarityIndex.ToString());
+
+        }
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 29.12.2020
+
+*/
