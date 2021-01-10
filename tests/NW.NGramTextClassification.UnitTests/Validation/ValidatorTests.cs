7710675aee0bea65ed36d8c1930dcd68f19e3a50
Rubèn <numbworks@gmail.com>
2021-01-10 23:40:23 +0100

    ValidatorTests, II iteration.

--- tests/NW.NGramTextClassification.UnitTests/Validation/ValidatorTests.cs
+++ tests/NW.NGramTextClassification.UnitTests/Validation/ValidatorTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+using System.Linq;
 
 namespace NW.NGramTextClassification.UnitTests
 {
@@ -8,7 +10,7 @@ namespace NW.NGramTextClassification.UnitTests
     {
 
         // Fields
-        private static TestCaseData[] validateArrayExceptionTextCases =
+        private static TestCaseData[] validateArrayExceptionTestCases =
         {
 
             new TestCaseData(
@@ -19,7 +21,7 @@ namespace NW.NGramTextClassification.UnitTests
                     ),
                 typeof(ArgumentNullException),
                 new ArgumentNullException(ObjectMother.VariableName).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateArrayExceptionTestCases)}_01"),
 
             new TestCaseData(
                 new TestDelegate(
@@ -29,20 +31,20 @@ namespace NW.NGramTextClassification.UnitTests
                     ),
                 typeof(ArgumentNullException),
                 new ArgumentNullException(MessageCollection.VariableContainsZeroItems.Invoke(ObjectMother.VariableName)).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateArrayExceptionTestCases)}_02"),
 
             new TestCaseData(
                 new TestDelegate(
                         () => Validator.ValidateArray<ArgumentException, string>(
-                                null, 
+                                null,
                                 ObjectMother.VariableName)
                     ),
                 typeof(ArgumentException),
                 new ArgumentException(ObjectMother.VariableName).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateArrayExceptionTestCases)}_03"),
 
         };
-        private static TestCaseData[] validateLengthExceptionTextCases =
+        private static TestCaseData[] validateLengthExceptionTestCases =
         {
 
             new TestCaseData(
@@ -52,7 +54,7 @@ namespace NW.NGramTextClassification.UnitTests
                 typeof(ArgumentException),
                 new ArgumentException(
                         MessageCollection.VariableCantBeLessThanOne.Invoke(ObjectMother.VariableName_Length)).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateLengthExceptionTestCases)}_01"),
 
             new TestCaseData(
                 new TestDelegate(
@@ -61,10 +63,10 @@ namespace NW.NGramTextClassification.UnitTests
                 typeof(Exception),
                 new Exception(
                         MessageCollection.VariableCantBeLessThanOne.Invoke(ObjectMother.VariableName_Length)).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateLengthExceptionTestCases)}_02")
 
         };
-        private static TestCaseData[] validateObjectExceptionTextCases =
+        private static TestCaseData[] validateObjectExceptionTestCases =
         {
 
             new TestCaseData(
@@ -73,7 +75,7 @@ namespace NW.NGramTextClassification.UnitTests
                     ),
                 typeof(ArgumentNullException),
                 new ArgumentNullException(ObjectMother.VariableName).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateObjectExceptionTestCases)}_01"),
 
             new TestCaseData(
                 new TestDelegate(
@@ -81,15 +83,25 @@ namespace NW.NGramTextClassification.UnitTests
                     ),
                 typeof(ArgumentException),
                 new ArgumentException(ObjectMother.VariableName).Message
-                ),
+                ).SetArgDisplayNames($"{nameof(validateObjectExceptionTestCases)}_02")
 
         };
+        private static TestCaseData[] allExceptionTestCases =
+                                            new List<TestCaseData>()
+                                                .Concat(validateArrayExceptionTestCases)
+                                                .Concat(validateLengthExceptionTestCases)
+                                                .Concat(validateObjectExceptionTestCases)
+                                                .ToArray();
 
         // SetUp
         // Tests
-        #region ValidateArrayTestMethods
-        [TestCaseSource(nameof(validateArrayExceptionTextCases))]
-        public void ValidateArray_ShouldThrowACertainException_WhenUnproperArguments
+        /*
+         * This one will work correctly in both NCrunch and Test Explorer 
+         * if using NUnit 3.13.0 and NUnit3 Adapter 3.17.0.
+         * 
+         */
+        [TestCaseSource(nameof(allExceptionTestCases))]
+        public void ValidateMethod_ShouldThrowACertainException_WhenUnproperArguments
             (TestDelegate del, Type expectedType, string expectedMessage)
         {
 
@@ -100,6 +112,7 @@ namespace NW.NGramTextClassification.UnitTests
             Assert.AreEqual(expectedMessage, actual.Message);
 
         }
+
         [Test]
         public void ValidateArray_ShouldDoNothing_WhenProperArgument()
         {
@@ -122,21 +135,7 @@ namespace NW.NGramTextClassification.UnitTests
             }
 
         }
-        #endregion
 
-        #region ValidateLengthTestMethods
-        [TestCaseSource(nameof(validateLengthExceptionTextCases))]
-        public void ValidateLength_ShouldThrowACertainException_WhenUnproperArguments
-            (TestDelegate del, Type expectedType, string expectedMessage)
-        {
-
-            // Arrange
-            // Act
-            // Assert
-            Exception actual = Assert.Throws(expectedType, del);
-            Assert.AreEqual(expectedMessage, actual.Message);
-
-        }
         [Test]
         public void ValidateLength_ShouldDoNothing_WhenProperArgument()
         {
@@ -159,21 +158,7 @@ namespace NW.NGramTextClassification.UnitTests
             }
 
         }
-        #endregion
 
-        #region ValidateObjectTestMethods
-        [TestCaseSource(nameof(validateObjectExceptionTextCases))]
-        public void ValidateObject_ShouldThrowACertainException_WhenUnproperArguments
-            (TestDelegate del, Type expectedType, string expectedMessage)
-        {
-
-            // Arrange
-            // Act
-            // Assert
-            Exception actual = Assert.Throws(expectedType, del);
-            Assert.AreEqual(expectedMessage, actual.Message);
-
-        }
         [Test]
         public void ValidateObject_ShouldDoNothing_WhenProperArgument()
         {
@@ -196,7 +181,6 @@ namespace NW.NGramTextClassification.UnitTests
             }           
 
         }
-        #endregion
 
         // TearDown
         // Support methods
