1140fa348bd0d7371252be74f5f633f709a66863
Rubèn <numbworks@gmail.com>
2021-01-23 01:03:15 +0100

    BigramTests: added.

---
+++ tests/NW.NGramTextClassification.UnitTests/NGrams/BigramTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace NW.NGramTextClassification.UnitTests
+{
+    [TestFixture]
+    public class BigramTests
+    {
+
+        // Fields
+        // SetUp
+        // Tests
+        [Test]
+        public void Bigram_ShouldCreateAnInstanceOfThisType_WhenProperArgument()
+        {
+
+            // Arrange
+            // Act
+            Bigram actual1
+                = new Bigram(
+                        new TokenizationStrategy(),
+                        ObjectMother.LabeledExample_Text1_BigramValue1
+                    );
+            Bigram actual2
+                = new Bigram(
+                        ObjectMother.LabeledExample_Text1_BigramValue1
+                    );
+
+            // Assert
+            Assert.IsInstanceOf<Bigram>(actual1);
+            Assert.IsInstanceOf<Bigram>(actual2);
+
+        }
+
+        // TearDown
+        // Support methods
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 22.01.2021
+
+*/
