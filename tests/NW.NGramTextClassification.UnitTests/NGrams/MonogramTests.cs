4d0207046f769c76a31b433aabe4ca1a37bc5e60
Rubèn <numbworks@gmail.com>
2021-01-23 01:01:51 +0100

    MonogramTests: added.

---
+++ tests/NW.NGramTextClassification.UnitTests/NGrams/MonogramTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace NW.NGramTextClassification.UnitTests
+{
+    [TestFixture]
+    public class MonogramTests
+    {
+
+        // Fields
+        // SetUp
+        // Tests
+        [Test]
+        public void Monogram_ShouldCreateAnInstanceOfThisType_WhenProperArgument()
+        {
+
+            // Arrange
+            // Act
+            Monogram actual1
+                = new Monogram(
+                        new TokenizationStrategy(),
+                        ObjectMother.LabeledExample_Text1_MonogramValue1
+                    );
+            Monogram actual2
+                = new Monogram(
+                        ObjectMother.LabeledExample_Text1_MonogramValue1
+                    );
+
+            // Assert
+            Assert.IsInstanceOf<Monogram>(actual1);
+            Assert.IsInstanceOf<Monogram>(actual2);
+
+        }
+
+        // TearDown
+        // Support methods
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 22.01.2021
+
+*/
