8cf91b0b1d86b37b622e6d1784c23abd41811457
Rubèn <numbworks@gmail.com>
2021-01-23 01:04:49 +0100

    TrigramTests: added.

---
+++ tests/NW.NGramTextClassification.UnitTests/NGrams/TrigramTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace NW.NGramTextClassification.UnitTests
+{
+    [TestFixture]
+    public class TrigramTests
+    {
+
+        // Fields
+        // SetUp
+        // Tests
+        [Test]
+        public void Trigram_ShouldCreateAnInstanceOfThisType_WhenProperArgument()
+        {
+
+            // Arrange
+            // Act
+            Trigram actual1
+                = new Trigram(
+                        new TokenizationStrategy(),
+                        ObjectMother.LabeledExample_Text1_TrigramValue1
+                    );
+            Trigram actual2
+                = new Trigram(
+                        ObjectMother.LabeledExample_Text1_TrigramValue1
+                    );
+
+            // Assert
+            Assert.IsInstanceOf<Trigram>(actual1);
+            Assert.IsInstanceOf<Trigram>(actual2);
+
+        }
+
+        // TearDown
+        // Support methods
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 22.01.2021
+
+*/
