3847fcd01e99b9f459ea47b29df3388c48219d0b
Rubèn <numbworks@gmail.com>
2021-01-26 22:51:27 +0100

    TextClassifierSettingsTests: added.

---
+++ tests/NW.NGramTextClassification.UnitTests/TextClassifierSettingsTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace NW.NGramTextClassification.UnitTests
+{
+    [TestFixture]
+    public class TextClassifierSettingsTests
+    {
+
+        // Fields
+        // SetUp
+        // Tests
+        [Test]
+        public void TextClassifierSettings_ShouldCreateAnInstanceOfThisType_WhenProperArgument()
+        {
+
+            // Arrange
+            // Act
+            TextClassifierSettings actual1 = new TextClassifierSettings();
+            TextClassifierSettings actual2 = new TextClassifierSettings(10);
+
+            // Assert
+            Assert.IsInstanceOf<TextClassifierSettings>(actual1);
+            Assert.IsInstanceOf<TextClassifierSettings>(actual2);
+
+        }
+
+        // TearDown
+        // Support methods
+
+    }
+}
+
+/*
+
+    Author: numbworks@gmail.com
+    Last Update: 26.01.2021
+
+*/
