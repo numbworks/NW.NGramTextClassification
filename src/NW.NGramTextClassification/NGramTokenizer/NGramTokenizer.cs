1ffd3973b7d6e634fe706c94d8b4d06732780a2d
Rubèn <numbworks@gmail.com>
2021-05-01 02:11:32 +0200

    NGramTokenizer: fixed "System.ArgumentException: The 'length''s value is greater or equal than 'arr.Length''s value.".

--- src/NW.NGramTextClassification/NGramTokenizer/NGramTokenizer.cs
+++ src/NW.NGramTextClassification/NGramTokenizer/NGramTokenizer.cs
@@ -44,65 +44,6 @@ namespace NW.NGramTextClassification
             => Do(text, new TokenizationStrategy(), new NGramTokenizerRuleSet());
 
         // Methods (private)
-        private T CreateInstance<T>(params object[] args)
-            => (T)Activator.CreateInstance(typeof(T), args);
-        private ushort GetN<T>()
-            => ((INGram)CreateInstance<T>(new TokenizationStrategy(), "whatever_value")).N;
-        private List<T> GetTokens<T>(MatchCollection matches, ITokenizationStrategy strategy)
-        {
-
-            // "This", "is", ..., "text" => ["This", "is", ..., "text"]
-            string[] allWords = new string[matches.Count];
-            for (int i = 0; i < matches.Count; i++)
-                allWords[i] = matches[i].Value;
-
-            ushort N = GetN<T>();
-
-            List<T> tokens = new List<T>();
-            for (uint i = 0; i < allWords.Length; i++)
-            {
-
-                // The last x NGrams are shorter in length...
-                ushort currentN = N;
-                if ((allWords.Length - i) < N)
-                    currentN = (ushort)(allWords.Length - i);
-
-                // For N = 3: ["This", "is", "a"], ["is", "a", "sample"], ...
-                string[] currentSubset = _ArrayManager.GetSubset(allWords, i, currentN);
-
-                // [ "This", "is", "a" ] => [ "This", " ", "is", " ", "a" ]
-                currentSubset = _ArrayManager.AddDelimiter(currentSubset, strategy.Delimiter);
-
-                // [ "This", " ", "is", " ", "a" ] => "This is a"
-                StringBuilder currentToken = new StringBuilder();
-                foreach (string word in currentSubset)
-                    currentToken.Append(word);
-
-                // "This is a" => "this is a"
-                if (strategy.ToLowercase)
-                    currentToken = new StringBuilder(currentToken.ToString().ToLower());
-
-                // new Monogram(strategy, "this is a")
-                T nGram = CreateInstance<T>(strategy, currentToken.ToString());
-
-                tokens.Add(nGram);
-
-            }
-
-            return tokens;
-
-        }
-        private List<T> DoFor<T>(string text, ITokenizationStrategy strategy) where T : INGram
-        {
-
-            // "This is a sample text." => "This", "is", ..., "text"
-            MatchCollection matches = Regex.Matches(text, strategy.Pattern);
-            if (matches.Count == 0)
-                throw new Exception(MessageCollection.NGramsTokenizer_ProvidedTokenizationStrategyPatternReturnsZeroMatches.Invoke(strategy));
-
-            return GetTokens<T>(matches, strategy);
-
-        }
         private List<INGram> TokenizeText
             (string text, ITokenizationStrategy strategy, INGramTokenizerRuleSet ruleSet)
         {
@@ -154,6 +95,71 @@ namespace NW.NGramTextClassification
             }
 
         }
+        private List<T> DoFor<T>(string text, ITokenizationStrategy strategy) where T : INGram
+        {
+
+            // "This is a sample text." => "This", "is", ..., "text"
+            MatchCollection matches = Regex.Matches(text, strategy.Pattern);
+            if (matches.Count == 0)
+                throw new Exception(MessageCollection.NGramsTokenizer_ProvidedTokenizationStrategyPatternReturnsZeroMatches.Invoke(strategy));
+
+            return GetTokens<T>(matches, strategy);
+
+        }
+        private List<T> GetTokens<T>(MatchCollection matches, ITokenizationStrategy strategy)
+        {
+
+            string[] allWords = ConvertToArray(matches);
+            ushort N = GetN<T>();
+
+            List<T> tokens = new List<T>();
+            for (uint i = 0; i < allWords.Length; i++)
+            {
+
+                // The last x NGrams are shorter in length...
+                ushort currentN = N;
+                if ((allWords.Length - i) < N)
+                    currentN = (ushort)(allWords.Length - i);
+
+                // For N = 3: ["This", "is", "a"], ["is", "a", "sample"], ...
+                string[] currentSubset = _ArrayManager.GetSubset(allWords, i, currentN);
+
+                // [ "This", "is", "a" ] => [ "This", " ", "is", " ", "a" ]
+                currentSubset = _ArrayManager.AddDelimiter(currentSubset, strategy.Delimiter);
+
+                // [ "This", " ", "is", " ", "a" ] => "This is a"
+                StringBuilder currentToken = new StringBuilder();
+                foreach (string word in currentSubset)
+                    currentToken.Append(word);
+
+                // "This is a" => "this is a"
+                if (strategy.ToLowercase)
+                    currentToken = new StringBuilder(currentToken.ToString().ToLower());
+
+                // new Monogram(strategy, "this is a")
+                T nGram = CreateInstance<T>(strategy, currentToken.ToString());
+
+                tokens.Add(nGram);
+
+            }
+
+            return tokens;
+
+        }
+        private string[] ConvertToArray(MatchCollection matches)
+        {
+
+            string[] allWords = new string[matches.Count];
+            for (int i = 0; i < matches.Count; i++)
+                allWords[i] = matches[i].Value;
+
+            return allWords;
+
+        }
+        private ushort GetN<T>()
+            => ((INGram)CreateInstance<T>(new TokenizationStrategy(), "whatever_value")).N;
+        private T CreateInstance<T>(params object[] args)
+            => (T)Activator.CreateInstance(typeof(T), args);
 
     }
 }
