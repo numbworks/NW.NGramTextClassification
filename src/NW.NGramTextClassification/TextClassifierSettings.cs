271566e5018e455888646f8982c00ba233a5147d
Rubèn <numbworks@gmail.com>
2021-09-18 14:12:36 +0200

    TextClassifierSettings: added summaries.

--- src/NW.NGramTextClassification/TextClassifierSettings.cs
+++ src/NW.NGramTextClassification/TextClassifierSettings.cs
@@ -1,34 +1,43 @@
 ﻿namespace NW.NGramTextClassification
 {
+    /// <summary>Collects all the global settings required by this library.</summary>
     public class TextClassifierSettings
     {
 
-        // Fields
-        // Properties
+        #region Fields
+        #endregion
+
+        #region Properties
+
         public static uint DefaultTruncateTextInLogMessagesAfter { get; } = 20;
 
         public uint TruncateTextInLogMessagesAfter { get; private set; }
 
-        // Constructors
-        public TextClassifierSettings(
-            uint truncateTextInLogMessagesAfter)
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="TextClassifierSettings"/> instance.</summary>
+        public TextClassifierSettings(uint truncateTextInLogMessagesAfter)
         {
 
             TruncateTextInLogMessagesAfter = truncateTextInLogMessagesAfter;
 
         }
+
+        /// <summary>Initializes a <see cref="TextClassifierSettings"/> instance using default parameters.</summary>
         public TextClassifierSettings()
             : this(DefaultTruncateTextInLogMessagesAfter) { }
 
-        // Methods (public)
-        // Methods (private)
+        #endregion
+
+        #region Methods_public
+        #endregion
 
     }
 }
 
 /*
-
     Author: numbworks@gmail.com
-    Last Update: 04.01.2021
-
+    Last Update: 18.09.2021
 */
