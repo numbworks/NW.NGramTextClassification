9adbc6e8582d4b940e49e331526fbcadd6bf2734
Rubèn <numbworks@gmail.com>
2022-09-28 00:14:11 +0200

    #6 NW.NGramTextClassificationClient: added ApplicationAbout namespace.

---
+++ src/NW.NGramTextClassificationClient/ApplicationAbout/AboutManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using NW.NGramTextClassification.Validation;
+using NW.NGramTextClassificationClient.Shared;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace NW.NGramTextClassificationClient.ApplicationAbout
+{
+    /// <inheritdoc cref="IAboutManager"/>
+    public class AboutManager : IAboutManager
+    {
+
+        #region Fields
+
+        private ILibraryBroker _libraryBroker;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="AboutManager"/> instance.</summary>
+        /// <exception cref="ArgumentNullException"/>
+        public AboutManager(ILibraryBroker libraryBroker)
+        {
+
+            Validator.ValidateObject(libraryBroker, nameof(libraryBroker));
+
+            _libraryBroker = libraryBroker;
+
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        public CommandLineApplication Add(CommandLineApplication app)
+        {
+
+            Validator.ValidateObject(app, nameof(app));
+
+            app.Command(Shared.MessageCollection.About_Name, command =>
+            {
+
+                command = AddMain(command);
+
+            });
+
+            return app;
+
+        }
+
+        #endregion
+
+        #region Methods_private
+
+        private CommandLineApplication AddMain(CommandLineApplication app)
+        {
+
+            app.Description = Shared.MessageCollection.About_Description;
+            app.OnExecute(() =>
+            {
+
+                return _libraryBroker.RunAboutMain();
+
+            });
+
+            return app;
+
+        }
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 27.09.2022
+*/
---
+++ src/NW.NGramTextClassificationClient/ApplicationAbout/IAboutManager.cs
@@ -0,0 +1,18 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace NW.NGramTextClassificationClient.ApplicationAbout
+{
+    /// <summary>Represents the <c>About</c> command of the CLI application.</summary>
+    public interface IAboutManager
+    {
+
+        /// <summary>Add the <c>About</c> command to the CLI application.</summary>
+        CommandLineApplication Add(CommandLineApplication app);
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 27.09.2022
+*/
