a67b027175411e3bca5326a49fc6da2199b17b6e
Rubèn <numbworks@gmail.com>
2022-09-28 01:05:40 +0200

    #6 NW.NGramTextClassificationClient.UnitTests: added ApplicationSession namespace.

---
+++ tests/NW.NGramTextClassificationClient.UnitTests/ApplicationSession/SessionManagerComponentsTests.cs
@@ -0,0 +1,46 @@
+﻿using NW.NGramTextClassificationClient.ApplicationSession;
+using NUnit.Framework;
+
+namespace NW.NGramTextClassificationClient.UnitTests
+{
+    [TestFixture]
+    public class SessionManagerComponentsTests
+    {
+
+        #region Fields
+        #endregion
+
+        #region SetUp
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void SessionManagerComponents_ShouldCreateAnObjectOfTypeSessionManagerComponents_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            SessionManagerComponents actual
+                    = new SessionManagerComponents();
+
+            // Assert
+            Assert.IsInstanceOf<SessionManagerComponents>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+        #endregion
+
+        #region Support_methods
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 27.09.2022
+*/
---
+++ tests/NW.NGramTextClassificationClient.UnitTests/ApplicationSession/SessionManagerTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using NW.NGramTextClassificationClient.ApplicationSession;
+using NW.NGramTextClassificationClient.Shared;
+using NUnit.Framework;
+
+namespace NW.NGramTextClassificationClient.UnitTests
+{
+    [TestFixture]
+    public class SessionManagerTests
+    {
+
+        #region Fields
+
+        private static TestCaseData[] sessionManagerExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new SessionManager(null, new SessionManagerComponents())
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("libraryBroker").Message
+            ).SetArgDisplayNames($"{nameof(sessionManagerExceptionTestCases)}_01"),
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new SessionManager(new LibraryBroker(), null)
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("sessionManagerComponents").Message
+            ).SetArgDisplayNames($"{nameof(sessionManagerExceptionTestCases)}_02")
+
+        };
+        private static TestCaseData[] addExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate(
+                    () => new SessionManager(
+                                libraryBroker: new LibraryBroker(),
+                                sessionManagerComponents: new SessionManagerComponents()
+                            ).Add(null)
+                ),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("app").Message
+            ).SetArgDisplayNames($"{nameof(addExceptionTestCases)}_01")
+
+        };
+
+        #endregion
+
+        #region SetUp
+        #endregion
+
+        #region Tests
+
+        [TestCaseSource(nameof(sessionManagerExceptionTestCases))]
+        public void SessionManager_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        [TestCaseSource(nameof(addExceptionTestCases))]
+        public void Add_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        [Test]
+        public void SessionManager_ShouldCreateAnObjectOfTypeSessionManager_WhenInvoked()
+        {
+
+            // Arrange
+            // Act
+            SessionManager actual
+                = new SessionManager(
+                        libraryBroker: new LibraryBroker(),
+                        sessionManagerComponents: new SessionManagerComponents());
+
+            // Assert
+            Assert.IsInstanceOf<SessionManager>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+        #endregion
+
+        #region Support_methods
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 27.09.2022
+*/
