e8f7e4ac97e4bea6ac377eccc1f313d2ac2b696b
Rubèn <numbworks@gmail.com>
2022-10-02 21:00:05 +0200

    #10 LabeledExampleSerializer, I*: added - Iteration 1.

---
+++ src/NW.NGramTextClassification/LabeledExamples/LabeledExampleSerializer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NW.NGramTextClassification.LabeledExamples
+{
+    /// <inheritdoc cref="ILabeledExampleSerializer"/>
+    public class LabeledExampleSerializer : ILabeledExampleSerializer
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="LabeledExampleSerializer"/> instance.</summary>
+        public LabeledExampleSerializer() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public string SerializeToJson(List<LabeledExample> labeledExamples)
+        {
+
+            Validation.Validator.ValidateList(labeledExamples, nameof(labeledExamples));
+
+            string json = "";
+
+            return json;
+
+        }
+        public List<LabeledExample> DeserializeFromJson(string json)
+        {
+
+            return null;
+
+        }
+
+        #endregion
+
+        #region Methods_private
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 02.10.2022
+*/
