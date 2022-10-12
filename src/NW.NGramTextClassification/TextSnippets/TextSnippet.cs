493ba9dae217d9f1e4046c03c144e6f9c0af7227
Rubèn <numbworks@gmail.com>
2022-10-12 09:53:50 +0200

    #11 TextSnippet, *Tests: added.

---
+++ src/NW.NGramTextClassification/TextSnippets/TextSnippet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NW.NGramTextClassification.Validation;
+
+namespace NW.NGramTextClassification.TextSnippets
+{
+    /// <summary>A snippet of text.</summary>
+    public class TextSnippet
+    {
+
+        #region Fields
+
+        #endregion
+
+        #region Properties
+
+        public string Text { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="TextSnippet"/> instance using default parameters.</summary>
+        /// <exception cref="ArgumentNullException"/>
+        public TextSnippet(string text) 
+        {
+
+            Validator.ValidateStringNullOrWhiteSpace(text, nameof(text));
+
+            Text = text;
+
+        }
+
+        #endregion
+
+        #region Methods_public
+
+        public override string ToString()
+        {
+
+            return $"[ {nameof(Text)}: '{Text}' ]";
+
+        }
+
+        #endregion
+
+        #region Methods_private
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 12.10.2022
+*/
---
+++ tests/NW.NGramTextClassification.UnitTests/TextSnippets/TextSnippetTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NW.NGramTextClassification.TextSnippets;
+using NUnit.Framework;
+
+namespace NW.NGramTextClassification.UnitTests.TextSnippets
+{
+    [TestFixture]
+    public class TextSnippetTests
+    {
+
+        #region Fields
+
+        private static TestCaseData[] textSnippetExceptionTestCases =
+        {
+
+            new TestCaseData(
+                new TestDelegate( () => new TextSnippet(text: null)),
+                typeof(ArgumentNullException),
+                new ArgumentNullException("text").Message
+                ).SetArgDisplayNames($"{nameof(textSnippetExceptionTestCases)}_01")
+
+        };
+
+        #endregion
+
+        #region SetUp
+
+        #endregion
+
+        #region Tests
+
+        [TestCaseSource(nameof(textSnippetExceptionTestCases))]
+        public void TextSnippet_ShouldThrowACertainException_WhenUnproperArguments
+            (TestDelegate del, Type expectedType, string expectedMessage)
+                => Utilities.ObjectMother.Method_ShouldThrowACertainException_WhenUnproperArguments(del, expectedType, expectedMessage);
+
+        [Test]
+        public void ToString_ShouldReturnExpectedString_WhenInvoked()
+        {
+
+            // Arrange
+            string text = "Some text";
+            string expected = $"[ {nameof(TextSnippet.Text)}: '{text}' ]";
+
+            // Act
+            string actual = new TextSnippet(text: text).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void TextSnippet_ShouldCreateAnInstanceOfThisType_WhenProperArgument()
+        {
+
+            // Arrange
+            // Act
+            TextSnippet actual = new TextSnippet(text: "Some text");
+
+            // Assert
+            Assert.IsInstanceOf<TextSnippet>(actual);
+
+        }
+
+        #endregion
+
+        #region TearDown
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 12.10.2022
+*/
