0cf31c6569d3522811a8917e0f618e293bcd15d0
Rubèn <numbworks@gmail.com>
2022-10-12 19:56:21 +0200

    #12 FilenameFactory, I*: added.

---
+++ src/NW.NGramTextClassification/Filenames/FilenameFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using NW.NGramTextClassification.Validation;
+
+namespace NW.NGramTextClassification.Filenames
+{
+    /// <inheritdoc cref="IFilenameFactory"/>
+    public class FilenameFactory : IFilenameFactory
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+
+        public static string DefaultFileNameTemplate { get; } = "{0}_{1}_{2}.{3}";
+        public static string DefaultMainToken { get; } = "ngramtc";
+        public static string DefaultTextSnippetsToken { get; } = "textsnippets";
+        public static string DefaultLabeledExamplesToken { get; } = "labeledexamples";
+        public static string DefaultSessionToken { get; } = "session";
+        public static string DefaultFormatNow { get; } = "yyyyMMddHHmmssfff";
+        public static string DefaultJsonExtension { get; } = "json";
+
+        #endregion
+
+        #region Constructors
+
+        ///<summary>Initializes a <see cref="FilenameFactory"/> instance.</summary>
+        public FilenameFactory() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public string CreateForTextSnippetsJson(string filePath, DateTime now)
+            => ValidateAndCreate(filePath, DefaultMainToken, DefaultTextSnippetsToken, now, DefaultJsonExtension);
+        public string CreateForLabeledExamplesJson(string filePath, DateTime now)
+            => ValidateAndCreate(filePath, DefaultMainToken, DefaultLabeledExamplesToken, now, DefaultJsonExtension);
+        public string CreateForSessionJson(string filePath, DateTime now)
+            => ValidateAndCreate(filePath, DefaultMainToken, DefaultSessionToken, now, DefaultJsonExtension);
+
+        #endregion
+
+        #region Methods_private
+
+        private string ValidateAndCreate(string filePath, string mainToken, string secondaryToken, DateTime now, string extension)
+        {
+
+            Validator.ValidateStringNullOrWhiteSpace(filePath, nameof(filePath));
+
+            string template = DefaultFileNameTemplate;
+            string nowstring = now.ToString(DefaultFormatNow);
+
+            string fileName = string.Format(template, mainToken, secondaryToken, nowstring, extension);
+
+            return Path.Combine(filePath, fileName);
+
+        }
+
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 12.10.2022
+*/
---
+++ src/NW.NGramTextClassification/Filenames/IFilenameFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NW.NGramTextClassification.Filenames
+{
+    /// <summary>Collects all the methods related to create filenames for this library.</summary>
+    public interface IFilenameFactory
+    {
+
+        /// <summary>
+        /// Returns a dated filename based on <paramref name="filePath"/> and <see cref="FilenameFactory.DefaultLabeledExamplesToken"/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        string CreateForLabeledExamplesJson(string filePath, DateTime now);
+
+        /// <summary>
+        /// Returns a dated filename based on <paramref name="filePath"/> and <see cref="FilenameFactory.DefaultSessionToken"/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>        
+        string CreateForSessionJson(string filePath, DateTime now);
+
+        /// <summary>
+        /// Returns a dated filename based on <paramref name="filePath"/> and <see cref="FilenameFactory.DefaultTextSnippetsToken"/>.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>           
+        string CreateForTextSnippetsJson(string filePath, DateTime now);
+    
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 12.10.2022
+*/
