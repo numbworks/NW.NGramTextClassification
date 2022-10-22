715c080487ac0fdd5f4a5225c158cb5f1b4b19ff
Rubèn <numbworks@gmail.com>
2022-10-22 22:04:24 +0200

    #15 DoubleManager, I*: added.

---
+++ src/NW.NGramTextClassificationClient/ApplicationSession/DoubleManager.cs
@@ -0,0 +1,58 @@
+﻿namespace NW.NGramTextClassificationClient.ApplicationSession
+{
+    /// <inheritdoc cref="IDoubleManager"/>
+    public class DoubleManager : IDoubleManager
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a <see cref="DoubleManager"/> instance.</summary>	
+        public DoubleManager() { }
+
+        #endregion
+
+        #region Methods_public
+
+        public bool IsValid(string value)
+        {
+
+            try
+            {
+                double parsed = double.Parse(value);
+
+                return true;
+
+            }
+            catch
+            {
+
+                return false;
+
+            }
+
+        }
+        public bool IsWithinRange(double value)
+        {
+
+            return value >= 0.0 && value <= 1.0;
+
+        }
+
+        #endregion
+
+        #region Methods_private
+        #endregion
+
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 22.10.2022
+*/
---
+++ src/NW.NGramTextClassificationClient/ApplicationSession/IDoubleManager.cs
@@ -0,0 +1,23 @@
+﻿namespace NW.NGramTextClassificationClient.ApplicationSession
+{
+    /// <summary>Collects all the utility methods related to <see cref="double"/> option values.</summary>
+    public interface IDoubleManager
+    {
+
+        /// <summary>
+        /// Establishes if the <see cref="uint"/> option value is valid or not.
+        /// </summary>
+        bool IsValid(string value);
+
+        /// <summary>
+        /// Returns true if <paramref name="value"/> is between 0.0 and 1.0.
+        /// </summary>        
+        bool IsWithinRange(double value);
+    
+    }
+}
+
+/*
+    Author: numbworks@gmail.com
+    Last Update: 22.10.2022
+*/
