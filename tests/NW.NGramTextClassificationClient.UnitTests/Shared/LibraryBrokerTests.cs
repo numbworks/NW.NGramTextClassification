a0b105f163c3fdf72e2f1badf104987e4390951a
Rubèn <numbworks@gmail.com>
2022-10-23 13:42:43 +0200

    #15 LibraryBrokerTests, added: RunSessionClassify_ShouldReturnFailureAndLogException_WhenClassifyDataIsNull().

--- tests/NW.NGramTextClassificationClient.UnitTests/Shared/LibraryBrokerTests.cs
+++ tests/NW.NGramTextClassificationClient.UnitTests/Shared/LibraryBrokerTests.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 using NW.NGramTextClassificationClient.Shared;
 using NW.NGramTextClassification.NGramTokenization;
+using NW.NGramTextClassification.UnitTests.Utilities;
 using NUnit.Framework;
+using System.Collections.Generic;
+using NW.NGramTextClassification;
+using NW.NGramTextClassification.Similarity;
+using NW.NGramTextClassification.LabeledExamples;
+using NW.NGramTextClassification.AsciiBanner;
+using NW.NGramTextClassification.Files;
+using NW.NGramTextClassification.Serializations;
+using NW.NGramTextClassification.Filenames;
+using NW.NGramTextClassificationClient.UnitTests.Utilities;
 
 namespace NW.NGramTextClassificationClient.UnitTests
 {
@@ -79,9 +89,71 @@ namespace NW.NGramTextClassificationClient.UnitTests
 
         }
 
+        [Test]
+        public void RunSessionClassify_ShouldReturnFailureAndLogException_WhenClassifyDataIsNull()
+        {
+
+            // Arrange
+            (List<string> messages, List<string> messagesAsciiBanner, TextClassifierComponents fakeComponents) = CreateTuple();
+
+            LibraryBroker libraryBroker
+                = new LibraryBroker(
+                        componentsFactory: new FakeTextClassifierComponentsFactory(fakeComponents),
+                        settingsFactory: new TextClassifierSettingsFactory(),
+                        textClassifierFactory: new TextClassifierFactory()
+                    );
+
+            // Act
+            int actual = libraryBroker.RunSessionClassify(null);
+
+            // Assert
+            Assert.AreEqual(LibraryBroker.Failure, actual);
+            Assert.AreEqual(
+                    expected: LibraryBroker.ErrorMessageFormatter(new ArgumentNullException("classifyData").Message),
+                    actual: messages[0]
+                    );
+            Assert.AreEqual(
+                    expected: LibraryBroker.SeparatorLine,
+                    actual: messagesAsciiBanner[0]
+                    );
+
+        }
+
         #endregion
 
         #region TearDown
+        #endregion
+
+        #region Support_methods
+
+        private (List<string>, List<string>, TextClassifierComponents) CreateTuple()
+        {
+
+            List<string> messages = new List<string>();
+            Action<string> fakeLoggingAction = (message) => messages.Add(message);
+
+            List<string> messagesAsciiBanner = new List<string>();
+            Action<string> fakeLoggingActionAsciiBanner = (message) => messagesAsciiBanner.Add(message);
+
+            TextClassifierComponents components = new TextClassifierComponents(
+
+                          nGramsTokenizer: new NGramTokenizer(),
+                          similarityIndexCalculator: new SimilarityIndexCalculatorJaccard(),
+                          roundingFunction: TextClassifierComponents.DefaultRoundingFunction,
+                          textTruncatingFunction: TextClassifierComponents.DefaultTextTruncatingFunction,
+                          loggingAction: fakeLoggingAction,
+                          labeledExampleManager: new LabeledExampleManager(),
+                          asciiBannerManager: new AsciiBannerManager(),
+                          loggingActionAsciiBanner: fakeLoggingActionAsciiBanner,
+                          fileManager: new FileManager(),
+                          serializerFactory: new SerializerFactory(),
+                          filenameFactory: new FilenameFactory(),
+                          nowFunction: TextClassifierComponents.DefaultNowFunction);
+
+            return (messages, messagesAsciiBanner, components);
+
+        }
+
         #endregion
 
     }
