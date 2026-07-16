84d74b90d5dce795253ba8da6243a465cf1e8183
numbworks <numbworks@gmail.com>
2026-07-16 19:16:29 +0000

    #35 Added named container.

---
+++ data/TemplateClass/__Class__.cs
@@ -0,0 +1,22 @@
+namespace __NAMESPACE__
+{
+    /// <summary>Represents a __Class__.</summary>
+    public class __Class__
+    {
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        /// <summary> Initializes a new instance of the <see cref="__Class__"/> class.</summary>
+        public __Class__() { }
+        #endregion
+
+        #region Methods (public)
+        #endregion
+
+    }
+}
